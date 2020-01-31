310073d4c3d172b9cd1ae313898c151b8c62f2c9
dimaematveev <dimaematveev@gmail.com>
2020-01-31 16:48:39 +0300

    Строка

---
+++ ClassAndStructure/String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ddd = "dwefewfwefwef";
+            const long COUNT = 10000;
+
+            string[] strs = new string[COUNT];
+            for (int i = 0; i < COUNT; i++)
+            {
+                strs[i] = ddd;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
